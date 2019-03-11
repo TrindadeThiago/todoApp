05fbfd9620bde3f86e2b7a264b9a85ea407ef7cc
Thiago Trindade <thiago.trindadexd@gmail.com>
2019-03-10 22:40:49 -0300

    fist commit

---
+++ src/js/task.js
@@ -0,0 +1,46 @@
+let tasks = [];
+
+function idGenerator() {
+    var timestamp = new Date();
+
+    var id =
+        timestamp.getHours().toString +
+        timestamp.getMinutes().toString +
+        timestamp.getSeconds().toString +
+        timestamp.getMilliseconds().toString;
+
+    return id;
+}
+
+function createTask(taskDescription) {
+    var task = {
+        id: idGenerator(),
+        data: {
+            description: taskDescription
+        }
+    };
+
+    tasks.push(task);
+
+    updateScreen();
+}
+
+function updateScreen() {
+    var list = "<ul>";
+
+    tasks.forEach((task =>{
+        list += "<li id-data="+task.id+">"+task.data.description+"</li>";
+        list += "<button onclick=deleteTask(this) id-data=" + task.id + ">Apagar</button>"
+    }));
+
+    list += "</ul>";
+    
+    document.getElementById("list").innerHTML = list;
+    document.getElementById("newTask").value = "";
+}
+
+function deleteTask(id) {
+    tasks = tasks.filter(task=>task.id!=id);
+
+    updateScreen();
+}
